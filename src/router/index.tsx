35bf20f98c2fd67d3685f888f7402570cd32694a
cxqdmm <940909315@qq.com>
2020-11-26 10:52:46 +0800

    change title

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,39 +1,39 @@
-import home from '../md/home/index.md';
-import GerberaMd from '../md/gerbera/index.md';
-import GerberaView from '../md/gerbera';
-import { Switch, Redirect, BrowserRouter } from 'react-router-dom';
-import { renderRouterWithChildren } from './routeWithLayout';
-import Layout from '../components/Layout';
-import React from 'react';
-import MdView from '../components/MdView';
-import { IRoute } from './interface';
+import home from "../md/home/index.md";
+import GerberaMd from "../md/gerbera/index.md";
+import GerberaView from "../md/gerbera";
+import { Switch, Redirect, BrowserRouter } from "react-router-dom";
+import { renderRouterWithChildren } from "./routeWithLayout";
+import Layout from "../components/Layout";
+import React from "react";
+import MdView from "../components/MdView";
+import { IRoute } from "./interface";
 
-const WEB_ROOT = process.env.WEB_ROOT || '/';
+const WEB_ROOT = process.env.WEB_ROOT || "/";
 
 function getPath(path: string) {
   return `${WEB_ROOT}${path}`;
 }
 
-export const homePath = getPath('/home');
+export const homePath = getPath("/home");
 const routes: IRoute[] = [
   {
     path: WEB_ROOT,
     layout: Layout,
-    layoutProps: { title: 'react 解析' },
+    layoutProps: { title: "flowers" },
     ignoreCache: true,
     children: [
       {
         path: homePath,
-        name: 'home',
-        component: () => <MdView md={home} />,
+        name: "home",
+        component: () => <MdView md={home} />
       },
       {
-        path: getPath('/gerbera'),
-        name: 'useEffect',
-        component: () => <MdView md={GerberaMd} view={<GerberaView />} />,
-      },
-    ],
-  },
+        path: getPath("/gerbera"),
+        name: "gerbera",
+        component: () => <MdView md={GerberaMd} view={<GerberaView />} />
+      }
+    ]
+  }
 ];
 
 const RootRouter = () => {
